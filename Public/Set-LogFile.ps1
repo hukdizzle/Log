a38ebb7b09379d9b5f6abd121dcb43d8c1128cc0
Curtis Jones <hukdizzle@users.noreply.github.com>
2017-03-25 11:37:45 -0700

    Final logic changes in place, everything is working as intended.

--- Public/Set-LogFile.ps1
+++ Public/Set-LogFile.ps1
@@ -4,14 +4,14 @@
     .NOTES
 	    Title:			Set-LogFile.ps1
 	    Author:			Curtis Jones
-	    Date:			March 10th, 2017
+	    Date:			March 24th, 2017
 	    Version:		1.0.0
 	    Requirements:		Powershell 3.0
 	
     .SYNOPSIS
-        Facilitates log function generation in the $env:LOCALAPPDATA\PowerShellLogging folder and also manages log file size threshold and renaming of old log files.
+        Provides log file generation in the $env:LOCALAPPDATA\PowerShellLogging folder based on input from the Add-Log function.
     .DESCRIPTION
-        Set-LogFile provides an effective way to create and manage log files within the $env:LOCALAPPDATA\PowerShellLogging folder. This function requires a log file name string and log file threshold integer input with an optional switch parameter to show the log file location. Set-LogFile will attempt to create the log file path if necessary and establish a global log file size threshold and logfile path to be used by Add-Log. If the ShowLocation switch is used the MessageBox show method will display the location of the generated log file.
+        Set-LogFile provides an easy and effective way to create a new log file if necessary in the $env:LOCALAPPDATA\PowerShellLogging folder. If necessary the path and the file will be created and a file path string will be returned by the function for use by the Add-Log function. If the ShowLocation switch param is used the MessageBox show method will be used to display the location of the created file.
     .PARAMETER LogFileName
 
         Provide a simple name with no spaces such as < ScriptName >
@@ -21,7 +21,7 @@
     .EXAMPLE
         Set-LogFile -LogFileName TestLogName -ShowLocation
 
-        The example above will create the following log file/location, $env:LOCALAPPDATA\PowerShellLogging\TestLogName\TestLogName.log. The log file size threshold will be set to 10MB and the location of the log file will be displayed using the MessageBox show method.
+        The example above will create the following log file/location, $env:LOCALAPPDATA\PowerShellLogging\TestLogName\TestLogName.log. The log file location will be displayed using the MessageBox show method.
     #>
     [CmdletBinding()]
     [OutputType([string])]
@@ -34,9 +34,9 @@
     )
     process
     {
-        #A logPath string variable is set utilizing an IO.Path method to combine the local AppData, PowerShellLogging, and the logFileName param after it's been sanitized of any extensions or invalid characters that may have been erroneously input to the function.
+        #A logPath string variable is set utilizing an IO.Path method to combine the local AppData path, PowerShellLogging string, and the logFileName param after it's been sanitized of any extensions or invalid characters that may have been erroneously input to the function.
 
-        $logPath = ([IO.Path]::Combine($env:LOCALAPPDATA,'PowerShellLogging',[IO.Path]::GetFileNameWithoutExtension($logFileName)))
+        $logPath = ([System.IO.Path]::Combine($env:LOCALAPPDATA,'PowerShellLogging',[System.IO.Path]::GetFileNameWithoutExtension($logFileName)))
         
         #If the logPath location does not exist the following block will be entered and the directory will be created. If creation fails a terminating error will be thrown.
         
@@ -52,9 +52,9 @@
             }
         }
         
-        #A global logFile string variable will be constructed utilzing the logPath string, sanitized logFileName param input, and all necessary delimiters and extensions. A global logThreshold variable will be set based on the logFileSizeThreshold function input.
+        #A logFile string variable will be constructed utilzing the logPath string, sanitized logFileName param input, and all necessary delimiters and extensions. If the logFile file path does not exist it will be created by the try/catch.
 
-        [string]$logFile = $logPath + '\' + $([IO.Path]::GetFileNameWithoutExtension($logFileName)) + ".log"
+        [string]$logFile = $logPath + '\' + $([System.IO.Path]::GetFileNameWithoutExtension($logFileName)) + ".log"
 
         if(-not (Test-Path $logFile))
         {
@@ -68,7 +68,7 @@
             }
         }
 
-        #If the ShowLocation param is used the following block will be entered. The Win32 Forms assembly will be loaded to expose the MessageBox class and methods. The Show method is used to pop up a Windows forms box to indicate the log file location and the user must acknowledge this by clicking on the OK button within the message box dialog.
+        #If the ShowLocation param is used and the executing invocation is running in UserInteractive mode the following block will be entered. The Win32 Forms assembly will be loaded to expose the MessageBox class and methods. The Show method is used to pop up a Windows forms box to indicate the log file location and the user must acknowledge this by clicking on the OK button within the message box dialog.
 
         if($ShowLocation -and ([Environment]::UserInteractive))
         {
@@ -76,6 +76,9 @@
             [System.Windows.Forms.MessageBox]::Show("Please refer to the log file located at the following location.`r`n`n$($logFile)","Log File Setup") | Out-Null
         }
     }
+    
+    #The logFile file path string will be returned to the calling code.
+
     end
     {
         return $logFile
